eaee196f839387818f9e0a80c2157242e56a81a2
amineelakkermi <amineelakkermi37@gmail.com>
2025-07-29 23:00:54 +0100

    mise a jour

--- components/Philosophy.jsx
+++ components/Philosophy.jsx
@@ -18,7 +18,7 @@ const Philosophy = () => {
       className="w-full relative snap-start bg-black1 text-white py-12 lg:py-20 px-6 lg:px-20 overflow-hidden"
     >
       {/* صور موزعة بمواقع مختلفة */}
-      <div className="absolute inset-0 w-full h-full z-20 pointer-events-none opacity-30">
+      <div className="absolute hidden inset-0 w-full h-full z-20 pointer-events-none opacity-30">
         <div className="relative w-full h-full">
           {positions.map((pos, i) => (
             <Image
