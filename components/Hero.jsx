1df49b3be2476b32e48f421fbbe7d67bf384b704
amineelakkermi <amineelakkermi37@gmail.com>
2025-07-30 16:27:23 +0100

    mise a jour

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -6,8 +6,21 @@ import titanium2 from "../public/images/titanium2.png";
 
 const Hero = () => {
   return (
-    <section className="relative svg w-full min-h-screen bg-black text-white flex items-center justify-center overflow-hidden">
-   
+    <section className="relative w-full min-h-screen bg-black text-white flex items-center justify-center overflow-hidden">
+      {/* Image de fond */}
+      <Image
+        src={titanium}
+        alt="background"
+        fill
+        quality={100}
+        priority
+        className="object-cover object-center opacity-60"
+      />
+
+      <div className="gradient-hero"></div>
+
+      {/* Overlay Titanium Glint */}
+      <div className="titanium-bg-glint absolute inset-0 z-10"></div>
 
       {/* Contenu animé */}
       <div className="relative z-20 max-w-5xl w-full px-6 lg:px-16 py-8 lg:py-12">
