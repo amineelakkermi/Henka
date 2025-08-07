354f4019ff404f9187e21c4de2037de910e07c1b
amineelakkermi <amineelakkermi37@gmail.com>
2025-08-07 01:23:54 +0100

    mise a jour

--- components/ThemeToggle.jsx
+++ components/ThemeToggle.jsx
@@ -2,12 +2,12 @@
 
 import { useEffect, useState } from 'react';
 import { useTheme } from '@/context/ThemeContext';
+import { FiSun, FiMoon } from 'react-icons/fi'; // 👈 استيراد الأيقونات
 
 export default function ThemeToggle() {
   const { theme, toggleTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
 
-  // Only show the toggle after mounting to avoid hydration mismatch
   useEffect(() => {
     setMounted(true);
   }, []);
@@ -21,13 +21,13 @@ export default function ThemeToggle() {
   return (
     <button
       onClick={() => toggleTheme(isDark ? 'light' : 'dark')}
-      className="p-2 rounded-full focus:outline-none  focus:ring-offset-2 focus:ring-offset-black focus:ring-white"
+      className="p-2 rounded-full  focus:outline-none focus:ring-offset-2 focus:ring-offset-black focus:ring-white transition duration-200 hover:scale-105"
       aria-label={isDark ? 'Switch to light mode' : 'Switch to dark mode'}
     >
       {isDark ? (
-        <span className="text-yellow-300">☀️</span>
+        <FiSun className="text-yellow-300 text-[36px] md:text-[22px]" />
       ) : (
-        <span className="text-gray-700">🌙</span>
+        <FiMoon className="text-white text-[36px] md:text-[22px]" />
       )}
     </button>
   );
